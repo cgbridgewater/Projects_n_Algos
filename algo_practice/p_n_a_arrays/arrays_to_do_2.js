5ca7a9939949c3e895564b9db716366a2520b7dc
cgbridgewater <cgbridgewater@outlook.com>
2023-04-17 12:18:59 -0700

    adding algos

--- algo_practice/p_n_a_arrays/arrays_to_do_2.js
+++ algo_practice/p_n_a_arrays/arrays_to_do_2.js
@@ -2,16 +2,39 @@
 
 
 // Reverse
-// Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second 
-// array – move values within the array that you are given. As always, do not use built-in array functions such as splice().copy
+// Given a numerical array, reverse the order of values, in-place. The reversed array should have the same length, with existing elements moved to other indices so that order of elements is reversed. Working 'in-place' means that you cannot use a second array – move values within the array that you are given. As always, do not use built-in array functions such as splice().copy
+
+
+function reverse(arr){
+    for(let i = 0; i< arr.length/2; i++ ){
+        var temp = arr[i]
+        arr[i] = arr[arr.length-1-i]
+        arr[arr.length-1-i] = temp
+    }
+return arr
+}
+
+console.log( reverse([1,2,3,4,5,6,7,8]) );
+console.log( reverse([1,2,3,4,5,6,7,8,9,10,11]) );
+
+
 // Rotate
 // Implement rotateArr(arr, shiftBy) that accepts array and offset. Shift arr's values to the right by that amount. 'Wrap-around' any values that shift off array's end to the other side, so that no data is lost. Operate in-place: given ([1,2,3],1), 
 // change the array to [3,1,2]. Don't use built-in functions.
 // Second: allow negative shiftBy (shift L, not R).
 // Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
 // Fourth: minimize the touches of each element.copy
+
+
+
+
+
+
 // Filter Range
 // Alan is good at breaking secret codes. One method is to eliminate values that lie outside of a specific known range. Given arr and values min and max, retain only the array values between min and max. Work in-place: return the array you are given, with values in original order. No built-in array functions.copy
+
+
+
 // Concat
 
 
