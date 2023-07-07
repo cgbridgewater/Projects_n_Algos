01027b0cd9fbccdbcb8e70e60e3735c04fdd3922
Chris Bridgewater <cgbridgewater@outlook.com>
2023-07-07 11:56:24 -0700

    clean up

--- algo_practice/p_n_a_arrays/arrays_to_do_1.js
+++ algo_practice/p_n_a_arrays/arrays_to_do_1.js
@@ -1,105 +1,99 @@
-
-
-
 // Push Front
 // Given an array and an additional value, insert this value at the beginning of the array. You may use .push(), you are able do this without it though!
 
-// function pushFront(arr,val){
-//     arr.push(val)
-//     for( var i= arr.length-1 ; i>0 ; i--)
-//         if( arr[i] == val){
-//             temp = arr[i]
-//             arr[i] = arr[i-1]
-//             arr[i-1] = temp
-//         }
-//     return arr
-// }
-// console.log(result =  pushFront([6,7,5,3,0,9],8));
-// console.log(result =  pushFront([5,7,2,3],8));
+function pushFront(arr,val){
+    arr.push(val)
+    for( var i= arr.length-1 ; i>0 ; i--)
+        if( arr[i] == val){
+            temp = arr[i]
+            arr[i] = arr[i-1]
+            arr[i-1] = temp
+        }
+    return arr
+}
+console.log(result =  pushFront([6,7,5,3,0,9],8));
+console.log(result =  pushFront([5,7,2,3],8));
 
 
-// function push(arr,val){
-//     for(let i = arr.length; i >= 0; i-- ){
-//         arr[i] = arr[i-1]
-//     }
+function push(arr,val){
+    for(let i = arr.length; i >= 0; i-- ){
+        arr[i] = arr[i-1]
+    }
 
-//     arr[0] = val
+    arr[0] = val
 
-//     return arr
-// }
-// console.log(push([5,7,2,3],8));
+    return arr
+}
+console.log(push([5,7,2,3],8));
 
-// var arr = [1,2,3,4,5,6]
-// arr[6]= arr[5]
-// console.log(arr);
+var arr = [1,2,3,4,5,6]
+arr[6]= arr[5]
+console.log(arr);
 
 
 // Pop Front
 // Given an array, remove and return the value at the beginning of the array. Prove the value is removed from the array by printing it. You may use .pop(), you are able do this without it though!
 
 
-// function popFront(arr){
-//     var temp
-//     for(var i = 0; i < arr.length-1; i++){
-//         if( i == 0){
-//             temp = arr[0]
-//         }
-//         arr[i] = arr[i+1] 
-//     }
-//     arr[arr.length-1] = temp
-//     arr.pop()
-//     console.log(arr);
-//     return temp
-// }
+function popFront(arr){
+    let temp = arr[0]
+    for(var i = 0; i < arr.length-1; i++){
+        arr[i] = arr[i+1] 
+    }
+    arr[arr.length-1] = temp
+    arr.pop()
+    console.log(arr);
+    return temp
+}
 
-// console.log(popFront([0,5,10,15]));
-// console.log(popFront([4,5,7,9]));
+console.log(popFront([0,5,10,15]));
+console.log(popFront([4,5,7,9]));
 
-// function popFront(arr){
-//     let temp = arr[0];
-//     for (var i =0; i < arr.length-1; i++){
-//         arr[i] = arr[i+1];
-//     }
-//     arr.length= arr.length-1;
-//     console.log(arr);
-//     return temp
-// }
-// console.log(popFront([0,5,10,15]));
-// console.log(popFront([4,5,7,9]));
+function popFront(arr){
+    let temp = arr[0];
+    for (var i =0; i < arr.length-1; i++){
+        arr[i] = arr[i+1];
+    }
+    arr.length= arr.length-1;
+    console.log(arr);
+    return temp
+}
+console.log(popFront([0,5,10,15]));
+console.log(popFront([4,5,7,9]));
 
 
 // Insert At
 // Given an array, index, and additional value, insert the value into array at given index. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val). You may use .push(), you are able do this without it though!
 
-// Examples:
+Examples:
 
-// insertAt([100,200,5], 2, 311) => [100,200,311,5]
-// insertAt([9,33,7], 1, 42) => [9,42,33,7]
-
-
-// function insertAt(arr, loc, val){
-//     arr.push(val)
-//     for(var i = arr.length-1; i > loc; i-- )
-//         if(i != loc){
-//             temp = arr[i-1]
-//             arr[i-1] = arr[i]
-//             arr[i] = temp
-//         }
-//         return arr
-// }
-// console.log(result= insertAt([100,200,5], 2, 311));
-// console.log(result= insertAt([9,33,7], 1, 42));
+insertAt([100,200,5], 2, 311) //=> [100,200,311,5]
+insertAt([9,33,7], 1, 42) //=> [9,42,33,7]
 
 
-// function insertedAt(arr,loc,val){
-//     for(let i = arr.length; i >= loc; i--){
-//         arr[i] = arr[i-1]
-//     }
-//     arr[loc] = val 
-//     return arr
-// }
-// console.log(insertedAt([100,200,5], 2, 311));
-// console.log(insertedAt([9,33,7], 1, 42));
+function insertAt(arr, loc, val){
+    arr.push(val)
+    for(var i = arr.length-1; i > loc; i-- )
+        if(i != loc){
+            temp = arr[i-1]
+            arr[i-1] = arr[i]
+            arr[i] = temp
+        }
+        return arr
+}
+console.log(result= insertAt([100,200,5], 2, 311));
+console.log(result= insertAt([9,33,7], 1, 42));
+
+
+function insertedAt(arr,loc,val){
+    for(let i = arr.length; i >= loc; i--){
+        arr[i] = arr[i-1]
+    }
+    arr[loc] = val 
+    return arr
+}
+console.log(insertedAt([100,200,5], 2, 311));
+console.log(insertedAt([9,33,7], 1, 42));
 
 
 // BONUS: Remove At
@@ -107,45 +101,45 @@
 
 // Examples:
 
-// removeAt([1000,3,204,77], 1) => 3 returned, with [1000,204,77] printed in the function
-// removeAt([8,20,55,44,98], 3) => 44 returned, with [8,20,55,98] printed in the function
-// 
+removeAt([1000,3,204,77], 1) //=> 3 returned, with [1000,204,77] printed in the function
+removeAt([8,20,55,44,98], 3) //=> 44 returned, with [8,20,55,98] printed in the function
 
-// function removeAt(arr, idx){
-//     temp = arr[idx]
-//     for(let i = idx; i< arr.length; i++){
-//         arr[i] = arr[i+1]
-//     }
-//     arr.length--
-//     console.log(arr);
-//     return temp
-// }
 
-// console.log(removeAt([1000,3,204,77], 1));
-// console.log(removeAt([8,20,55,44,98], 3));
+function removeAt(arr, idx){
+    temp = arr[idx]
+    for(let i = idx; i< arr.length; i++){
+        arr[i] = arr[i+1]
+    }
+    arr.length--
+    console.log(arr);
+    return temp
+}
+
+console.log(removeAt([1000,3,204,77], 1));
+console.log(removeAt([8,20,55,44,98], 3));
 
 
 
 // BONUS: Swap Pairs
 // Swap positions of successive pairs of values of given array. If length is odd, do not change the final element.
 
-// Examples:
+Examples:
 
-// insertAt([1,2,3,4]) => [2,1,4,3]
-// insertAt(["Brendan",true,42]) => [true,"Brendan",42]
+insertAt([1,2,3,4]) //=> [2,1,4,3]
+insertAt(["Brendan",true,42])//=> [true,"Brendan",42]
 
-// function insertAt(arr){
-//     for (let i = 0; i<arr.length-1; i++){
-//         if (i % 2 == 0){
-//             var temp = arr[i]
-//             arr[i] = arr[i+1]
-//             arr[i+1] = temp
-//         }
-//     }
-//     return arr
-// }
-// console.log(insertAt([1,2,3,4]));
-// console.log(insertAt(["Brendan",true,42]));
+function insertAt(arr){
+    for (let i = 0; i<arr.length-1; i++){
+        if (i % 2 == 0){
+            var temp = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = temp
+        }
+    }
+    return arr
+}
+console.log(insertAt([1,2,3,4]));
+console.log(insertAt(["Brendan",true,42]));
 
 
 
@@ -154,8 +148,8 @@
 
 // Examples:
 
-// removeDupes([-2,-2,3.14,5,5,10]) => [-2,3.14,5,10]
-// removeDupes([9,19,19,19,19,19,29]) => [9,19,29]
+removeDupes([-2,-2,3.14,5,5,10]) //=> [-2,3.14,5,10]
+removeDupes([9,19,19,19,19,19,29])// => [9,19,29]
 
 
 
--- algo_practice/p_n_a_strings/strings_to_do_1.js
+++ algo_practice/p_n_a_strings/strings_to_do_1.js
@@ -4,114 +4,114 @@
 
 
 
-// // // // Remove Blanks
+// // // Remove Blanks
 // Create a function that, given a string, returns all of that string’s contents, but without blanks. 
 
 
-// // // short cut!
-// function removeBlanks(string){
-//     var noblanks = string.split(" ")
-//     console.log(noblanks.join(""))
-// }
+// // short cut!
+function removeBlanks(string){
+    var noblanks = string.split(" ")
+    console.log(noblanks.join(""))
+}
 
 
-// function removeBlanks(string){
-//     newString ='';
-//     for(let i =0; i< string.length; i++){
-//         if(string[i] !== ' '){
-//             newString += string[i]
-//         }
-//     }
-// console.log(newString);
-// }
-// // Examples:
-// removeBlanks(" Pl ayTha tF u nkyM usi c ")
-// removeBlanks("I can not BELIEVE it's not BUTTER")
-// //  => "PlayThatFunkyMusic"
-// //  => "IcannotBELIEVEit'snotBUTTER"
+function removeBlanks(string){
+    newString ='';
+    for(let i =0; i< string.length; i++){
+        if(string[i] !== ' '){
+            newString += string[i]
+        }
+    }
+console.log(newString);
+}
+// Examples:
+removeBlanks(" Pl ayTha tF u nkyM usi c ")
+removeBlanks("I can not BELIEVE it's not BUTTER")
+//  => "PlayThatFunkyMusic"
+//  => "IcannotBELIEVEit'snotBUTTER"
 
 
 
 
-// // // // Get Digits
+// // // Get Digits
 // Create a JavaScript function that given a string, returns the integer made from the string’s digits. You are allowed to use isNaN() and Number().
 
 
-// function getDigits(string){
-// var digits = "";
-//     for(let i = 0; i < string.length; i++){
-//         if( isNaN(Number(string[i])) ){
-//         }else{
-//             digits +=string[i]
-//         }
-//     }
-//     console.log(digits);
-// }
+function getDigits(string){
+var digits = "";
+    for(let i = 0; i < string.length; i++){
+        if( isNaN(Number(string[i])) ){
+        }else{
+            digits +=string[i]
+        }
+    }
+    console.log(digits);
+}
 
-// // Examples:
-// getDigits("abc8c0d1ngd0j0!8")
-// getDigits("0s1a3y5w7h9a2t4?6!8?0")
+// Examples:
+getDigits("abc8c0d1ngd0j0!8")
+getDigits("0s1a3y5w7h9a2t4?6!8?0")
 
 
-// // // // Acronyms
+// // // Acronyms
 // Create a function that, given a string, returns the string’s acronym (first letter of the word capitalized). You are allowed to use .split() and .toUpperCase().
 
 
-// function acronym(string){
-// var acronym = ''
-// wordArray = string.split(" ")
-// // console.log(wordArray);
-//     for(let i = 0; i < wordArray.length; i++){
-//         if(wordArray[i][0] == undefined){
+function acronym(string){
+var acronym = ''
+wordArray = string.split(" ")
+// console.log(wordArray);
+    for(let i = 0; i < wordArray.length; i++){
+        if(wordArray[i][0] == undefined){
             
-//         }else
-//             acronym += wordArray[i][0].toUpperCase()
-//     }
-//     console.log(acronym);
-// }
-// acronym(" there's no free lunch - gotta pay yer way. ")
-// acronym("Live from New York, it's Saturday Night!")
-// //  => "TNFL-GPYW". 
-// //   => "LFNYISN".
+        }else
+            acronym += wordArray[i][0].toUpperCase()
+    }
+    console.log(acronym);
+}
+acronym(" there's no free lunch - gotta pay yer way. ")
+acronym("Live from New York, it's Saturday Night!")
+//  => "TNFL-GPYW". 
+//   => "LFNYISN".
 
 
 
-// // // // Count Non-Spaces
+// // // Count Non-Spaces
 // Create a function that, given a string, returns the number of non-space characters found in the string. 
 
-// function countNonSpaces(string){
-//     count = 0;
-//     for(let i =0; i< string.length; i++){
-//         if(string[i] !== ' '){
-//             count++
-//         }
-//     }
-// console.log(count);
-// }
-// // Examples:
-// countNonSpaces("Honey pie, you are driving me crazy")
-// countNonSpaces("Hello world !")
-// //  => 29
-// //  => 11
+function countNonSpaces(string){
+    count = 0;
+    for(let i =0; i< string.length; i++){
+        if(string[i] !== ' '){
+            count++
+        }
+    }
+console.log(count);
+}
+// Examples:
+countNonSpaces("Honey pie, you are driving me crazy")
+countNonSpaces("Hello world !")
+//  => 29
+//  => 11
 
 
 
-// Remove Shorter Strings
+Remove Shorter Strings
 // Create a function that, given an array of strings and a numerical value, returns an array that only contains strings longer than or equal to the given value.
 
 
-// function removeShorterStrings(strings, val){
-//     const newArray = []
-//     for (let i = 0; i< strings.length; i++ ){
-//         if (strings[i].length >= val){ 
-//             newArray.push(strings[i])
-//         }
-//     }
-// return newArray
-// }
+function removeShorterStrings(strings, val){
+    const newArray = []
+    for (let i = 0; i< strings.length; i++ ){
+        if (strings[i].length >= val){ 
+            newArray.push(strings[i])
+        }
+    }
+return newArray
+}
 
-// // Examples:
-// console.log( removeShorterStrings(['Good morning', 'sunshine', 'the', 'Earth', 'says', 'hello'], 4) );
-// console.log( removeShorterStrings(['There', 'is', 'a', 'bug', 'in', 'the', 'system'], 3) );
-// // => ['Good morning', 'sunshine', 'Earth', 'says', 'hello']
-// // => ['There', 'bug', 'the', 'system']
+// Examples:
+console.log( removeShorterStrings(['Good morning', 'sunshine', 'the', 'Earth', 'says', 'hello'], 4) );
+console.log( removeShorterStrings(['There', 'is', 'a', 'bug', 'in', 'the', 'system'], 3) );
+// => ['Good morning', 'sunshine', 'Earth', 'says', 'hello']
+// => ['There', 'bug', 'the', 'system']
